000daf5f5a5823305e6c2d74f07642f4943736ac
prafulgabhane <prafulgabhane786@gmail.com>
2022-12-28 23:33:24 +0530

    Adding UserInput

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Program:");
+            UserInput.UserData();
+
+            Console.WriteLine("Program Ends Press Any Key");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorld/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace HelloWorld
+{
+	public class UserInput
+	{
+		public static void UserData()
+		{
+
+            Console.WriteLine("Enter Username:");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Username is: " + userName);
+
+        }
+    }
+}
+
