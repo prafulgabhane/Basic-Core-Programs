db61cb63085f94e47b905a1987137de48259f974
prafulgabhane <prafulgabhane786@gmail.com>
2022-12-30 23:05:36 +0530

    Adding code to remote

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,7 +8,7 @@
 
             Console.WriteLine("Choose any Program");
             Console.WriteLine("1:User Data\n2:Flip Coin\n3:Leap Year\n4:Power of Two\n5:Harmonic No");
-            //Console.WriteLine("");
+            Console.WriteLine("6:Quotient and Remainder\n");
 
             int choice = Convert.ToInt32(Console.ReadLine());
 
@@ -38,10 +38,10 @@
                     break;
 
                 case 6:
-                    Console.WriteLine("Harmonic Number");
+                    Console.WriteLine("Quotient and Remainder");
                     QuotientAndRemainder.Input();
                     break;
-                    
+                  
                 default:
                     Console.WriteLine("Enter Between Range");
                     break;
